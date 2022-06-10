c1e1c7e734b58b75da1649dc3c3c408a6c70c6b0
LiliwoL <github@liliwol.fr>
2022-06-10 16:24:34 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Analog Clock';
+  title: string = 'Horloge analogique';
 }
